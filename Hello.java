1b89fe888c5883d2eb4f68d5034ef17b87c3b9d4
naseray <59940314+naseray@users.noreply.github.com>
2020-01-15 17:50:25 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,23 @@
+
+/*********************************
+* Hello world program
+**********************************
+* Ahmad Naser
+* January 15, 2020
+*CMSC 255 Section 1
+**********************************/
+
+public class Hello {
+
+	public static void main(String[] args) {
+
+		System.out.println("Ahmad Naser");
+		System.out.println("Hello World");
+		System.out.println("First project");
+		System.out.println("January 15, 2020");
+		System.out.println("CMSC 255 Section 1");
+		System.out.println();
+
+	}
+
+}
